d8a6cdaf92f479000097907acfce6b02117e0915
Danila <zaharchuk.danila18@gmail.com>
2024-10-03 22:05:18 +0300

    WTPS-4 add footer

--- src/layouts/layoutDefault/LayoutDefault.vue
+++ src/layouts/layoutDefault/LayoutDefault.vue
@@ -2,11 +2,13 @@
     <div class="layout-default">
         <Header class="layout-default__header" />
         <router-view />
+        <Footer class="layout-default__footer"/>
     </div>
 </template>
 
 <script setup lang="ts">
     import Header from '~/components/header/Header.vue';
+    import Footer from '~/components/footer/Footer.vue';
 </script>
 <style scoped lang="scss">
   @import "./styles/layout-default";
